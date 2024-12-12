ab7827eb07dc2aaf9823ae093ac1953c5e384512
nikhilbawane <nikhilbawane03@gmail.com>
2024-12-12 19:13:48 +0530

    added gradient support for SliderTrackPainter

--- lib/src/painter/slider_track_painter.dart
+++ lib/src/painter/slider_track_painter.dart
@@ -1,21 +1,30 @@
+import 'package:circular_slider/circular_slider.dart';
 import 'package:flutter/rendering.dart';
 
 class SliderTrackPainter extends CustomPainter {
   final double offsetRadian;
   final double radius;
-  final double strokeWidth;
   final double startRadian;
   final double lengthRadian;
-  final Color color;
+  final double strokeWidth;
+  final StrokeCap? strokeCap;
+  final Color? color;
+  final List<Color>? gradientColors;
+  final List<double>? gradientStops;
+  final GradientMode gradientMode;
 
   const SliderTrackPainter({
     required this.offsetRadian,
     required this.radius,
-    required this.strokeWidth,
     required this.startRadian,
     required this.lengthRadian,
-    required this.color,
-  });
+    required this.strokeWidth,
+    this.strokeCap,
+    this.color,
+    this.gradientColors,
+    this.gradientStops,
+    this.gradientMode = GradientMode.arc,
+  }) : assert(color != null || gradientColors != null);
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -23,10 +32,40 @@ class SliderTrackPainter extends CustomPainter {
     final rect = Rect.fromCircle(center: center, radius: radius);
 
     final paint = Paint()
-      ..color = color
       ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = strokeWidth;
+      ..strokeCap = strokeCap ?? StrokeCap.round
+      ..strokeWidth = strokeWidth
+      ..isAntiAlias = true;
+
+    double capRadian = 0.0;
+
+    if (paint.strokeCap != StrokeCap.butt) {
+      capRadian = ((strokeWidth / 2) / radius) - 0.005;
+    }
+
+    if (gradientColors == null) {
+      paint.color = color!;
+    } else {
+      print('capRadian: $capRadian');
+
+      double gradientEndAngle;
+
+      if (gradientMode == GradientMode.circle) {
+        gradientEndAngle = startRadian + 2 * 3.141;
+      } else {
+        gradientEndAngle = startRadian + lengthRadian + capRadian;
+      }
+
+      final gradient = SweepGradient(
+        startAngle: startRadian - capRadian,
+        endAngle: gradientEndAngle,
+        colors: gradientColors!,
+        stops: gradientStops,
+        transform: GradientRotation(-capRadian),
+      );
+
+      paint.shader = gradient.createShader(rect);
+    }
 
     // Save the canvas state
     canvas.save();
